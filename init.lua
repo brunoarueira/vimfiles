62f3502ff413b903d79ee6c64866701546fce3ee
Bruno Arueira <contato@brunoarueira.com>
2023-02-02 16:12:24 -0300

    converts package management from vundle to packer

--- init.lua
+++ init.lua
@@ -8,61 +8,7 @@ vim.cmd([[
   syntax sync fromstart
   filetype off
 
-  set rtp+=~/.vim/bundle/Vundle.vim/
-
-  let uname = substitute(system('uname'), '\n', '', '')
-
-  if uname == 'Linux'
-    set rtp+=/home/linuxbrew/.linuxbrew/opt/fzf
-  else
-    set rtp+=/usr/local/opt/fzf
-  endif
-
-  call vundle#begin()
-
-  Plugin 'VundleVim/Vundle.vim'
-
-  Plugin 'ervandew/supertab'
-  Plugin 'vim-airline/vim-airline'
-  Plugin 'ryanoasis/vim-devicons'
-  Plugin 'danro/rename.vim'
-  Plugin 'godlygeek/tabular'
-  Plugin 'gorkunov/smartpairs.vim'
-  Plugin 'tomtom/tlib_vim'
-  Plugin 'MarcWeber/vim-addon-mw-utils'
-  Plugin 'int3/vim-extradite'
-  Plugin 'pangloss/vim-javascript'
-  Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
-  Plugin 'scrooloose/nerdtree'
-  Plugin 'ap/vim-css-color'
-  Plugin 'tomtom/tcomment_vim'
-  Plugin 'tpope/vim-abolish'
-  Plugin 'tpope/vim-endwise'
-  Plugin 'tpope/vim-fugitive'
-  Plugin 'tpope/vim-git'
-  Plugin 'tpope/vim-rails'
-  Plugin 'vim-ruby/vim-ruby'
-  Plugin 'vim-scripts/matchit.zip'
-  Plugin 'vim-scripts/gitignore'
-  Plugin 'bogado/file-line'
-  Plugin 'MaxMEllon/vim-jsx-pretty'
-  Plugin 'jasonshell/vim-svg-indent'
-  Plugin 'terryma/vim-multiple-cursors'
-  Plugin 'junegunn/fzf', { 'do': { -> fzf#install() } }
-  Plugin 'junegunn/fzf.vim'
-  Plugin 'victormours/ruby-memoize.vim'
-  Plugin 'jparise/vim-graphql'
-  Plugin 'mattn/emmet-vim'
-  Plugin 'w0rp/ale'
-  Plugin 'sourcegraph/javascript-typescript-langserver', {'for': ['javascript'], 'do': 'npm install && npm run build'}
-  Plugin 'arcticicestudio/nord-vim'
-  Plugin 'rust-lang/rust.vim'
-  " JSON front matter highlight plugin
-  Plugin 'elzr/vim-json'
-  Plugin 'plasticboy/vim-markdown'
-  Plugin 'editorconfig/editorconfig-vim'
-
-  call vundle#end()
+  lua require('plugins')
 
   filetype plugin indent on
 
---
+++ lua/plugins.lua
@@ -0,0 +1,52 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'ervandew/supertab'
+  use 'vim-airline/vim-airline'
+  use 'ryanoasis/vim-devicons'
+  use 'danro/rename.vim'
+  use 'godlygeek/tabular'
+  use 'gorkunov/smartpairs.vim'
+  use 'tomtom/tlib_vim'
+  use 'MarcWeber/vim-addon-mw-utils'
+  use 'int3/vim-extradite'
+  use 'pangloss/vim-javascript'
+
+  use { 'rstacruz/sparkup', rtp = 'vim/' }
+
+  use 'scrooloose/nerdtree'
+  use 'ap/vim-css-color'
+  use 'tomtom/tcomment_vim'
+  use 'tpope/vim-abolish'
+  use 'tpope/vim-endwise'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-git'
+  use 'tpope/vim-rails'
+  use 'vim-ruby/vim-ruby'
+  use 'vim-scripts/matchit.zip'
+  use 'vim-scripts/gitignore'
+  use 'bogado/file-line'
+  use 'MaxMEllon/vim-jsx-pretty'
+  use 'jasonshell/vim-svg-indent'
+  use 'terryma/vim-multiple-cursors'
+  use '/usr/local/opt/fzf'
+  use 'junegunn/fzf.vim'
+  use 'victormours/ruby-memoize.vim'
+  use 'jparise/vim-graphql'
+  use 'mattn/emmet-vim'
+  use 'w0rp/ale'
+
+  use {
+    'sourcegraph/javascript-typescript-langserver',
+    ft = 'javascript',
+    run = 'npm install && npm run build'
+  }
+
+  use 'arcticicestudio/nord-vim'
+  use 'rust-lang/rust.vim'
+
+  -- JSON front matter highlight use
+  use 'elzr/vim-json'
+  use 'plasticboy/vim-markdown'
+  use 'editorconfig/editorconfig-vim'
+end)
