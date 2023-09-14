acb858329b497b2fba27e402b55dfde2827c5c1b
Bruno Arueira <contato@brunoarueira.com>
2023-09-14 11:52:31 -0300

    updated theme config

--- lua/brunoarueira/theme.lua
+++ lua/brunoarueira/theme.lua
@@ -1,11 +1,32 @@
-local opt = vim.opt
+local api, o, cmd, highlight, opt = vim.api, vim.o, vim.cmd, vim.highlight, vim.opt
 
+-- o
+o.hidden = true
+o.emoji = true
+o.signcolumn = 'yes:1'
+o.completeopt = 'menu,menuone,noselect'
+o.shortmess = 'filnxtToOFc' -- Avoid showing extra messages when using completion
+o.formatoptions = 'cqrnj'
+o.termguicolors = true
+
+-- opt
 opt.showmode = false
 opt.sidescroll = 1
 opt.cf = true
 opt.incsearch = true
 opt.hlsearch = true
 opt.fillchars = ""
-opt.termguicolors = true
 
-vim.cmd.colorscheme('nord')
+-- cmd
+cmd.colorscheme('nord')
+
+-- Highlight yank'd text after yankin'
+api.nvim_create_augroup("YankHighlight", {})
+api.nvim_create_autocmd("TextYankPost", {
+	group = "YankHighlight",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 100 })
+	end,
+})
+
+
