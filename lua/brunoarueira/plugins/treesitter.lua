a9e25afb4f39a4357875750d36c378e01cf7a27a
Bruno Arueira <contato@brunoarueira.com>
2023-10-05 18:06:02 -0300

    adds html and json to treesitter config

--- lua/brunoarueira/plugins/treesitter.lua
+++ lua/brunoarueira/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 require('nvim-treesitter.configs').setup({
   ensure_installed = {
-    'ruby', 'javascript', 'vim', 'rust', 'yaml', 'lua', 'markdown', 'markdown_inline', 'bash'
+    'ruby', 'javascript', 'vim', 'rust', 'yaml', 'lua', 'markdown', 'markdown_inline', 'bash', 'html',
+    'json'
   },
   highlight = {
     enable = true,
