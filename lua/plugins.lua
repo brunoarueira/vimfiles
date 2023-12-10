982019a40a000d03ba91bfeb4d197228c6c231f7
Bruno Arueira <contato@brunoarueira.com>
2023-12-10 18:08:35 -0300

    improves install on new machines

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local status, packer = pcall(require, 'packer')
 
 if (not status) then
@@ -132,6 +145,12 @@ return packer.startup({
         require('brunoarueira.plugins.lsp')
       end
     }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      packer.sync()
+    end
   end,
   config = {
     display = {
