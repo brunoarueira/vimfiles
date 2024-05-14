9c65063f9b539a17b1037933463eca149e3e0e56
Bruno Arueira <contato@brunoarueira.com>
2024-05-14 19:36:36 -0300

    minor refactor

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,11 +1,13 @@
 -- Do not source the default filetypes.vim
 vim.cmd [[set updatetime=300]]
 
-vim.g.mapleader = ","
+local g, opt = vim.g, vim.opt
 
-vim.opt.shell = 'fish'
-vim.opt.shellquote = ''
-vim.opt.shellxquote = ''
+g.mapleader = ","
 
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
+opt.shell = 'fish'
+opt.shellquote = ''
+opt.shellxquote = ''
+
+g.do_filetype_lua = 1
+g.did_load_filetypes = 0
