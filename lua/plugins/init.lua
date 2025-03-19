1aa665e80dc68eaf98221ed1077cb0e90d744ae8
Bruno Arueira <contato@brunoarueira.com>
2025-03-19 10:59:17 -0300

    chore: improve vim-illuminate config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,8 +22,18 @@ return {
 
 	{
 		"RRethy/vim-illuminate",
-		config = function()
-			require("illuminate")
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			delay = 200,
+			filetypes_denylist = {
+				"dirbuf",
+				"dirvish",
+				"fugitive",
+				"NvimTree",
+			}
+		},
+		config = function(_, opts)
+			require("illuminate").configure(opts)
 
 			vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
 			vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
