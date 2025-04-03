3bee0e334ae63f260d2c520a6f6f6f3aaf4b8e90
Bruno Arueira <contato@brunoarueira.com>
2025-04-03 14:38:42 -0300

    refactor: move helpers to lua/plugins, it's only used there

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "kyazdani42/nvim-web-devicons" },
 		config = function()
-			require('brunoarueira.helpers')
+			require('plugins.helpers')
 
 			local colors = require('config.theme_colors')
 
