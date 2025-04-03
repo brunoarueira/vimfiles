306de8f5c86bc1faf012aaf3c4326d78c821f6d1
Bruno Arueira <contato@brunoarueira.com>
2025-04-03 14:35:35 -0300

    refactor: simplify css detection

--- ftdetect/css.lua
+++ ftdetect/css.lua
@@ -1,13 +1,9 @@
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-  pattern = "*.scss",
-  callback = function()
-    vim.bo.filetype = 'css'
-  end
-})
-
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-  pattern = "*.less",
-  callback = function()
-    vim.bo.filetype = 'css'
-  end
+	pattern = {
+		"*.scss",
+		"*.less",
+	},
+	callback = function()
+		vim.bo.filetype = 'css'
+	end
 })
